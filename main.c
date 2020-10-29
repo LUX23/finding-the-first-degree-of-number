040fe2c9546e20b46c51b86707bd607e0c42f89e
Matthew <70555513+LUX23@users.noreply.github.com>
2020-10-29 15:14:48 +0700

    Update main.c

--- main.c
+++ main.c
@@ -5,28 +5,33 @@
 // Дано целое число N. Определить первое i, для которого 3^i>N. Значение N
 // ввести в программу. Вывести значения N и i.
 
-int body(int N, int i)
+int work(int N)
 {
+    int i = 0;
     if (N > 0) // при положительных значениях N
     {
-    i = log(N) / log(3); // находим значение числа i, при котором 3^i>N
-    i = i + 1;
+
+        i = log(N) / log(3); // находим значение числа i, при котором 3^i>N
+
+        i = i + 1;
+
     }
     else
     i = 0; // при нуле и отрицательных числах, число i может быть любым;
            // но по условию нужно определить первое i
-    return(i);
+        return(i);
 }
 
 int main()
 {
     char *locale = setlocale(LC_ALL, "RUSSIAN");
-    
-    int N, i;
+
+    int N;
     printf("Введите значение числа N: ");
     scanf("%d", &N);
-    i = body(N, i);
+    i = work(N);
     printf("N=%d\ni=%d\n", N, i);
 
+    getch();
     return 0;
 }
