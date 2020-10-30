3fbc5fa03499411217b5b672dfa4a078d8667b9d
Matthew <70555513+LUX23@users.noreply.github.com>
2020-10-30 16:31:56 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 // Дано целое число N. Определить первое i, для которого 3^i>N. Значение N
 // ввести в программу. Вывести значения N и i.
-
 #include <stdio.h>
 #include <conio.h>
 #include <locale.h>
@@ -12,11 +11,8 @@ int find(int N)
 
     if (N > 0) // при положительных значениях N
     {
-
         i = log(N) / log(3); // находим значение числа i, при котором 3^i>N
-
         i = i + 1;
-
     }
     else
         i = 0; // при нуле и отрицательных числах, число i может быть любым;
@@ -27,11 +23,14 @@ int find(int N)
 int main()
 {
     char *locale = setlocale(LC_ALL, "RUSSIAN");
-
+    
     int N,i;
+    
     printf("Введите значение числа N: ");
     scanf("%d", &N);
+    
     i = find(N);
+    
     printf("N=%d\ni=%d\n", N, i);
 
     getch();
