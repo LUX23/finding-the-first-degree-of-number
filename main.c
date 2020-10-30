9558e314a3cad714f12feb5ad7f55390667f7f9b
Matthew <70555513+LUX23@users.noreply.github.com>
2020-10-30 16:10:19 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
-#include <math.h>
-#include <locale.h>
-
 // Дано целое число N. Определить первое i, для которого 3^i>N. Значение N
 // ввести в программу. Вывести значения N и i.
 
-int work(int N)
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#include <math.h>
+
+int find(int N)
 {
     int i = 0;
+
     if (N > 0) // при положительных значениях N
     {
 
@@ -17,19 +19,19 @@ int work(int N)
 
     }
     else
-    i = 0; // при нуле и отрицательных числах, число i может быть любым;
-           // но по условию нужно определить первое i
-        return(i);
+        i = 0; // при нуле и отрицательных числах, число i может быть любым;
+               // но по условию нужно определить первое i
+    return(i);
 }
 
 int main()
 {
     char *locale = setlocale(LC_ALL, "RUSSIAN");
 
-    int N;
+    int N,i;
     printf("Введите значение числа N: ");
     scanf("%d", &N);
-    i = work(N);
+    i = find(N);
     printf("N=%d\ni=%d\n", N, i);
 
     getch();
